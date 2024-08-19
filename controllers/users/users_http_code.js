fe031901b20f1e24c5de2c4a9f7a4128cd696c23
NQuocNk20 <quoc.nn00820@sinhvien.hoasen.edu.vn>
2024-08-19 22:09:40 +0700

    feat: create api create, update, get

--- controllers/users/users_http_code.js
+++ controllers/users/users_http_code.js
@@ -1,32 +1,35 @@
 const HTTP_STATUS_CODE = {
-    USER_CREATED_SUCCESSFUL: {
-      message: 'Create user successful',
-      status: 201,
-    },
-  
-    GET_USER_SUCCESSFUL: {
-      message: 'Get user successful',
-      status: 200,
-    },
-  
-    UPDATE_USER_SUCCESSFUL: {
-      message: 'Update user successful',
-      status: 204,
-    },
-  
-    DELETE_TASK_SUCCESSFUL: {
-      message: 'Delete task successful',
-      status: 204,
-    },
-    USER_AUTHENTICATED_FAILED: {
-      message: 'User authentication failed',
-      status: 401,
-    },
-    USER_AUTHENTICATED_SUCCESS: {
-      message: 'User authentication succeeded',
-      status: 200,
-    },
-  };
-  
-  module.exports = HTTP_STATUS_CODE;
-  
+  USER_CREATED_SUCCESSFUL: {
+    message: 'Create user successful',
+    status: 201,
+  },
+
+  GET_USER_SUCCESSFUL: {
+    message: 'Get user successful',
+    status: 200,
+  },
+
+  UPDATE_USER_SUCCESSFUL: {
+    message: 'Update user successful',
+    status: 204,
+  },
+
+  DELETE_TASK_SUCCESSFUL: {
+    message: 'Delete task successful',
+    status: 204,
+  },
+  USER_AUTHENTICATED_FAILED: {
+    message: 'User authentication failed',
+    status: 401,
+  },
+  USER_AUTHENTICATED_SUCCESS: {
+    message: 'User authentication succeeded',
+    status: 200,
+  },
+  SYSTEM_ERROR: {
+    message: 'SERVER_ERROR',
+    status: 500,
+  },
+};
+
+module.exports = HTTP_STATUS_CODE;
