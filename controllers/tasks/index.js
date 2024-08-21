eb2621a983a14d2613a427f9ca68be741935a20d
Tú Nguyễn Phạm Anh <nguyentu9962@gmail.com>
2024-08-21 21:09:55 +0700

    fix: status code

--- controllers/tasks/index.js
+++ controllers/tasks/index.js
@@ -15,9 +15,8 @@ exports.getUserTask = (request, response) => {
       tasksHttpCode.GET_TASK_SUCCESSFUL.message,
       response,
       data
-    )
+    );
     response.end();
-
   } catch (err) {
     helpers.writeResponse(
       tasksHttpCode.SYSTEM_ERROR.status,
@@ -30,7 +29,6 @@ exports.getUserTask = (request, response) => {
 };
 exports.createUserTask = (request, response) => {
   try {
-    debugger
     const data = helpers.readFileDataJson(tasksDataFilePath)
 
     let body = '';
@@ -43,27 +41,34 @@ exports.createUserTask = (request, response) => {
       let name = parsedBody.name;
       let isDone = parsedBody.isDone;
       let userId = parsedBody.userId;
-      task = {
-        taskId: helpers.generateUID(),
-        userId: userId,
-        name: name,
-        isDone: isDone
-      }
-
       if (name && isDone && userId) {
+        let task = {
+          taskId: helpers.generateUID(),
+          userId: userId,
+          name: name,
+          isDone: isDone
+        }
         data.push(task);
         helpers.writeFileDataJson(tasksDataFilePath, data)
+        helpers.writeResponse(
+          tasksHttpCode.TASK_CREATED_SUCCESSFUL.status,
+          tasksHttpCode.TASK_CREATED_SUCCESSFUL.message,
+          response,
+          data
+        )
+        response.end();
       }
-      helpers.writeResponse(
-        tasksHttpCode.TASK_CREATED_SUCCESSFUL.status,
-        tasksHttpCode.TASK_CREATED_SUCCESSFUL.message,
-        response,
-        data
-      )
-      response.end();
+      else {
+        helpers.writeResponse(
+          tasksHttpCode.BAD_REQUEST.status,
+          tasksHttpCode.BAD_REQUEST.message,
+          response,
+          []
+        );
+        response.end();
+      }
+
     });
-
-
   } catch (err) {
     helpers.writeResponse(
       tasksHttpCode.SYSTEM_ERROR.status,
@@ -76,7 +81,6 @@ exports.createUserTask = (request, response) => {
 };
 exports.updateUserTask = (request, response) => {
   try {
-    debugger
     const data = helpers.readFileDataJson(tasksDataFilePath)
 
     let body = '';
@@ -91,35 +95,43 @@ exports.updateUserTask = (request, response) => {
       let isDone = parsedBody.isDone;
       let userId = parsedBody.userId;
       let taskId = parsedBody.taskId;
-
-      task = {
-        taskId: taskId,
-        userId: userId,
-        name: name,
-        isDone: isDone
-      }
-
       if (name && isDone && userId && taskId) {
+        let task = {
+          taskId: taskId,
+          userId: userId,
+          name: name,
+          isDone: isDone
+        }
         let index = data.findIndex(data => data.taskId === taskId);
         if (index !== -1) {
           data[index] = task;
+          helpers.writeFileDataJson(tasksDataFilePath, data);
+          helpers.writeResponse(
+            tasksHttpCode.UPDATE_TASK_SUCCESSFUL.status,
+            tasksHttpCode.UPDATE_TASK_SUCCESSFUL.message,
+            response,
+            []
+          )
+          response.end();
         }
-        helpers.writeFileDataJson(tasksDataFilePath, data);
-        helpers.writeResponse(
-          tasksHttpCode.UPDATE_TASK_SUCCESSFUL.status,
-          tasksHttpCode.UPDATE_TASK_SUCCESSFUL.message,
-          response,
-          data
-        )
-        response.end();
+        else {
+          helpers.writeResponse(
+            tasksHttpCode.BAD_REQUEST.status,
+            tasksHttpCode.BAD_REQUEST.message,
+            response,
+            []
+          );
+          response.end();
+        }
+
       }
       else {
         helpers.writeResponse(
-          tasksHttpCode.UPDATE_TASK_SUCCESSFUL.status,
-          tasksHttpCode.UPDATE_TASK_SUCCESSFUL.message,
+          tasksHttpCode.BAD_REQUEST.status,
+          tasksHttpCode.BAD_REQUEST.message,
           response,
-          data
-        )
+          []
+        );
         response.end();
       }
 
@@ -137,7 +149,6 @@ exports.updateUserTask = (request, response) => {
 };
 exports.deleteUserTask = (request, response) => {
   try {
-    debugger
     const data = helpers.readFileDataJson(tasksDataFilePath)
     let body = '';
     let task = {};
@@ -147,27 +158,36 @@ exports.deleteUserTask = (request, response) => {
     request.on('end', () => {
       const parsedBody = JSON.parse(body);
       let taskId = parsedBody.taskId;
-      
-      if ( taskId) {
+
+      if (taskId) {
         let index = data.findIndex(data => data.taskId === taskId);
         if (index !== -1) {
           data.splice(index, 1);
+          helpers.writeFileDataJson(tasksDataFilePath, data);
+          helpers.writeResponse(
+            tasksHttpCode.UPDATE_TASK_SUCCESSFUL.status,
+            tasksHttpCode.UPDATE_TASK_SUCCESSFUL.message,
+            response,
+            []
+          );
+          response.end();
         }
-        helpers.writeFileDataJson(tasksDataFilePath, data);
-        helpers.writeResponse(
-          tasksHttpCode.UPDATE_TASK_SUCCESSFUL.status,
-          tasksHttpCode.UPDATE_TASK_SUCCESSFUL.message,
-          response,
-          data
-        )
-        response.end();
+        else {
+          helpers.writeResponse(
+            tasksHttpCode.TASK_NOT_FOUND.status,
+            tasksHttpCode.TASK_NOT_FOUND.message,
+            response,
+            []
+          );
+          response.end();
+        }
       }
       else {
         helpers.writeResponse(
           tasksHttpCode.SYSTEM_ERROR.status,
           tasksHttpCode.SYSTEM_ERROR.message,
           response,
-          [])
+          []);
         response.end();
       }
     });
@@ -177,7 +197,7 @@ exports.deleteUserTask = (request, response) => {
       tasksHttpCode.SYSTEM_ERROR.message,
       response,
       []
-    )
+    );
     response.end();
   }
 };
